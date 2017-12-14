115ccc00f17d0e0d0ecedd557002d27295da9d76
Ed Brannin <edbrannin@gmail.com>
2017-12-14 07:22:08 -0500

    Trying to list people in a group

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Based on https://gist.github.com/pklaus/1029870
+
 import objc
 import AddressBook as ab
 
 import pprint as pp
+
+GROUP_NAME = "Christmas 2017"
 
 def pythonize(objc_obj):
     if isinstance(objc_obj, objc.pyobjc_unicode):
@@ -25,7 +32,7 @@ def ab_person_to_dict(person, skip=None):
     return {prop.lower(): pythonize(person.valueForProperty_(prop))
             for prop in props if prop not in skip}
 
-def address_book_to_list():
+def address_book_group_to_list(group_name=GROUP_NAME):
     """
     Read the current user's AddressBook database, converting each person
     in the address book into a Dictionary of values. Some values (addresses,
@@ -36,7 +43,20 @@ def address_book_to_list():
     Function adapted from: https://gist.github.com/pklaus/1029870
     """
     address_book = ab.ABAddressBook.sharedAddressBook()
-    people = address_book.people()
-    return [ab_person_to_dict(person) for person in people]
+    groups = address_book.groups()
+    for group in groups:
+        if group.valueForProperty_("GroupName") == group_name:
+            break
+    pp.pprint(dir(group.members.selector))
+    pp.pprint(group.members.selector)
+    return
+    for person in group.members.selector:
+        pp.pprint(person)
+        yield ab_person_to_dict(person)
+    pp.pprint(group.members)
+    # people = address_book.people()
+    # return [ab_person_to_dict(person) for person in people]
 
-pp.pprint(address_book_to_list())
+if __name__ == '__main__':
+    for person in address_book_group_to_list():
+        pp.pprint(person)
